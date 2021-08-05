720d027c182525dcc53006832d8e9a84f9e12569
Ken Wang <kenuiuc@gmail.com>
2021-08-05 18:10:28 +0800

    working on renew exchange token

--- im-sdk-core/src/main/java/com/easemob/im/server/api/token/allocate/ExchangeTokenProvider.java
+++ im-sdk-core/src/main/java/com/easemob/im/server/api/token/allocate/ExchangeTokenProvider.java
@@ -12,12 +12,13 @@ import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
-import java.time.Instant;
 
 public class ExchangeTokenProvider implements TokenProvider {
 
     private static final Logger log = LoggerFactory.getLogger(ExchangeTokenProvider.class);
-    private static final int EXPIRE_SECONDS = 20;
+    private static final int EXPIRE_IN_SECONDS = 20;
+    private static final String DEMO_CLIENT_ID = "YXA66CvMXzNmTZanwZLekX6isA";
+    private static final String DEMO_CLIENT_SECRET = "YXA6WFIt5YLui0vy9GKUeX4PUMmoUgM";
 
     private final ExchangeTokenRequest exchangeTokenRequest = new ExchangeTokenRequest();
 
@@ -45,7 +46,7 @@ public class ExchangeTokenProvider implements TokenProvider {
         this.codec = codec;
         this.errorMapper = errorMapper;
         this.appToken = exchangeToken(properties.getAppId(), properties.getAppCert())
-                .cache(token -> Duration.between(Instant.now(), token.getExpireTimestamp())
+                .cache(token -> Duration.ofSeconds(EXPIRE_IN_SECONDS)
                                 .dividedBy(2),
                         error -> Duration.ofSeconds(10),
                         () -> Duration.ofSeconds(10));
@@ -58,18 +59,39 @@ public class ExchangeTokenProvider implements TokenProvider {
 
 
     private Mono<Token> exchangeToken(String appId, String appCert) {
-        String agoraAppTokenValue = AccessToken2Utils.buildAppToken(appId, appCert, EXPIRE_SECONDS);
+        String agoraAppTokenValue = AccessToken2Utils.buildAppToken(appId, appCert, EXPIRE_IN_SECONDS);
+        log.info("agoraAppTokenValue = {}", agoraAppTokenValue);
+
+        TokenRequest tokenRequest = AppTokenRequest.of(DEMO_CLIENT_ID, DEMO_CLIENT_SECRET);
         return endpointRegistry.endpoints()
                 .map(this.loadBalancer::loadBalance)
                 .flatMap(endpoint -> this.httpClient
-                        .baseUrl(String.format("%s/%s/token", endpoint.getUri(),
+                        .baseUrl(String.format("%s/%s", endpoint.getUri(),
                                 this.properties.getAppkeySlashDelimited()))
-                        .headers(headers -> headers.set("Authorization", String.format("Bearer %s", agoraAppTokenValue)))
+                        .headers(headers -> headers.set("agoraAppTokenValue", agoraAppTokenValue))
                         .post()
                         .uri("/token")
-                        .send(Mono.create(sink -> sink.success(this.codec.encode(exchangeTokenRequest))))
+                        .send(Mono.create(sink -> sink.success(this.codec.encode(tokenRequest))))
                         .responseSingle((rsp, buf) -> this.errorMapper.apply(rsp).then(buf)))
                 .map(buf -> this.codec.decode(buf, TokenResponse.class))
                 .map(TokenResponse::asToken);
+
+
+
+
+
+
+//        return endpointRegistry.endpoints()
+//                .map(this.loadBalancer::loadBalance)
+//                .flatMap(endpoint -> this.httpClient
+//                        .baseUrl(String.format("%s/%s/token", endpoint.getUri(),
+//                                this.properties.getAppkeySlashDelimited()))
+//                        .headers(headers -> headers.set("Authorization", String.format("Bearer %s", agoraAppTokenValue)))
+//                        .post()
+//                        .send(Mono.create(sink -> sink.success(this.codec.encode(exchangeTokenRequest))))
+//                        .responseSingle((rsp, buf) -> this.errorMapper.apply(rsp).then(buf)))
+//                .map(buf -> this.codec.decode(buf, TokenResponse.class))
+//                .map(TokenResponse::asToken);
     }
+
 }
